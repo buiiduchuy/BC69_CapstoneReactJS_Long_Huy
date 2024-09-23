7052b879c026ca2513a092c15a7f3faa84ca5953
NTLONG_VN <longnt2302@gmail.com>
2024-09-23 09:53:05 +0700

    update module delete phim by refetch

--- src/Components/templates/ListFilmAdminTemplate.tsx
+++ src/Components/templates/ListFilmAdminTemplate.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 
 export const ListFilmAdminTemplate = () => {
   // lấy danh sách phim
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ["DanhSachPhim"],
     queryFn: async () => {
       await sleep(1000);
@@ -24,23 +24,16 @@ export const ListFilmAdminTemplate = () => {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2OSIsIkhldEhhblN0cmluZyI6IjAxLzAyLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTczODM2ODAwMDAwMCIsIm5iZiI6MTcxMDUyMjAwMCwiZXhwIjoxNzM4NTE1NjAwfQ.ap-iPzMpXDeCuXH0aJnbbSuR3vIW4upk1nOK3h9D-5g",
   };
 
-  const [success, setSuccess] = useState<boolean>(false);
-
   return (
     <div className="sm:p-9 p-6 min-h-screen flex flex-col">
       <div className="flex flex-wrap justify-between md:mb-12 mb-5">
         <h2 className="text-white uppercase font-500 xl:text-[30px] md:text-[25px] text-[20px] mb-5 md:mb-0">
           Danh sách phim
         </h2>
-        <Button className="py-5 hover:!bg-orange-400 hover:!text-white hover:!border-white">
-          Thêm phim mới
-        </Button>
+        <Button className="py-5 hover:!bg-orange-400 hover:!text-white hover:!border-white">Thêm phim mới</Button>
       </div>
       <div className="mb-6">
-        <Input.Search
-          placeholder="Tìm phim..."
-          className="w-full adminInputSearch"
-        />
+        <Input.Search placeholder="Tìm phim..." className="w-full adminInputSearch" />
       </div>
       <table className="table-auto">
         <thead>
@@ -55,40 +48,36 @@ export const ListFilmAdminTemplate = () => {
         <tbody>
           {data?.data?.content?.map((phim) => {
             return (
-              <>
-                <tr key={phim.maPhim}>
-                  <td>{phim.tenPhim}</td>
-                  <td>{phim.maPhim}</td>
-                  <td>
-                    <img src={phim.hinhAnh} />
-                  </td>
-                  <td>{phim.maPhim}</td>
-                  <td>{phim.moTa}</td>
-                  <td>
-                    <Button
-                      danger
-                      onClick={async () => {
-                        console.log(phim.maPhim);
-                        try {
-                          await axios.delete(
-                            "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/XoaPhim?MaPhim=" +
-                              phim.maPhim,
-                            {
-                              headers,
-                            }
-                          );
-                          setSuccess(true);
-                        } catch (error) {
-                          console.log("🚀 ~ onClick={ ~ error:", error);
-                        }
-                      }}
-                    >
-                      Delete
-                    </Button>
-                    <Button type="primary">Edit</Button>
-                  </td>
-                </tr>
-              </>
+              <tr key={phim.maPhim}>
+                <td>{phim.tenPhim}</td>
+                <td>{phim.maPhim}</td>
+                <td>
+                  <img src={phim.hinhAnh} />
+                </td>
+                <td>{phim.maPhim}</td>
+                <td>{phim.moTa}</td>
+                <td>
+                  <Button
+                    danger
+                    onClick={async () => {
+                      try {
+                        await axios.delete(
+                          "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/XoaPhim?MaPhim=" + phim.maPhim,
+                          {
+                            headers,
+                          }
+                        );
+                        refetch();
+                      } catch (error) {
+                        console.log("🚀 ~ onClick={ ~ error:", error);
+                      }
+                    }}
+                  >
+                    Delete
+                  </Button>
+                  <Button type="primary">Edit</Button>
+                </td>
+              </tr>
             );
           })}
         </tbody>
